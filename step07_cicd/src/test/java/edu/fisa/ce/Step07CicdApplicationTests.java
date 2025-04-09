eb49c7cae12bde22c29f3b09aa0eb581fa3ed269
Jeongho427 <fgh2585@gmail.com>
2025-04-09 10:28:40 +0900

    feat: test code 추가

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -10,4 +10,13 @@ class Step07CicdApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test  // JUnit 4
+    // @org.junit.jupiter.api.Test // JUnit 5
+    public void testAddition() {
+        int result = 2 + 3;
+        System.out.println("비교 전*****************");
+        assertEquals(5, result);  // 성공: 5 == 5
+        System.out.println("비교 후*****************");
+    }
+
 }
